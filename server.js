fa8d9932fb9c6cbb70e7b22306dac61762dea876
hontit <hointit@gmail.com>
2018-08-08 17:26:42 +0700

    no message

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
-console.log("server started");
+console.log("server started: " + port);
